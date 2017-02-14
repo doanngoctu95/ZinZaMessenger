86e7dbd6577743233c359cac6a0f38282bbc0321
Tu Doan <doanngoctu95@gmail.com>
2017-02-14 17:31:56 +0700

    change UI color part 2

--- ZinZa/app/src/main/java/chotot/prect/aptech/zinzamessenger/activity/LoginActivity.java
+++ ZinZa/app/src/main/java/chotot/prect/aptech/zinzamessenger/activity/LoginActivity.java
@@ -22,7 +22,7 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.login_activity);
+        setContentView(R.layout.activity_login);
         initView();
     }
 
